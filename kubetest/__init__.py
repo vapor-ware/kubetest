bae3578a84dfd9a33cd16e235ffe7cb9df83f386
Erick Daniszewski <edaniszewski@gmail.com>
2018-09-14 13:35:41 -0400

    update description

--- kubetest/__init__.py
+++ kubetest/__init__.py
@@ -2,7 +2,7 @@
 
 __title__ = 'kubetest'
 __version__ = '0.0.1'
-__description__ = ''
+__description__ = 'A Kubernetes integration test framework in Python.'
 __author__ = 'Vapor IO'
 __author_email__ = 'vapor@vapor.io'
 __url__ = 'https://github.com/vapor-ware/kubetest'

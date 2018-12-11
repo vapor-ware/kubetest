08888ef36f42ed2e47750acdd3ad98dcf7392a2d
Erick Daniszewski <edaniszewski@gmail.com>
2018-12-11 09:32:44 -0500

    Migrate to Jenkins CI (#86)
    * Add .jenkins file for new CI
    
    * typo
    
    * set credential
    
    * clean up release flow testing

--- bin/ci/check_version.sh
+++ bin/ci/check_version.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 pkg_version="${PKG_VERSION}"
-circle_tag="${CIRCLE_TAG}"
+ci_tag="${TAG_NAME}"
 
 
-if [ ! "${pkg_version}" ] && [ ! "${circle_tag}" ]; then
+if [ ! "${pkg_version}" ] && [ ! "${ci_tag}" ]; then
     echo "No version or tag specified."
     exit 1
 fi
 
-if [ "${pkg_version}" != "${circle_tag}" ]; then
-    echo "Versions do not match: pkg@${pkg_version} tag@${circle_tag}"
+if [ "${pkg_version}" != "${ci_tag}" ]; then
+    echo "Versions do not match: pkg@${pkg_version} tag@${ci_tag}"
     exit 1
 fi
 
-echo "Versions match: pkg@${pkg_version} tag@${circle_tag}"
+echo "Versions match: pkg@${pkg_version} tag@${ci_tag}"

ed7e154ba837e967d5379dbfe0ea8a0209ac514d
Erick Daniszewski <edaniszewski@gmail.com>
2019-07-26 09:32:30 -0400

    add support for HTTP POST to a pod

--- kubetest/response.py
+++ kubetest/response.py
@@ -2,6 +2,8 @@
 
 import json
 
+import urllib3
+
 
 class Response:
     """Response is a wrapper around the Kubernetes API's response data
@@ -35,6 +37,21 @@ class Response:
         Returns:
             dict: The JSON data.
         """
+        # This should generally be the case, since the primary source of Response
+        # instances are the HTTP methods on Pods (e.g. http_proxy_get), where
+        # `_preload_content=False`. Should that be set to True, it will not return
+        # the HTTPResponse but will instead return the response data formatted as
+        # the type specified in the `response_type` param.
+        #
+        # Note that I have found setting _preload_content to True (the default value
+        # for the Kubernetes client) to be difficult to deal with from a generic interface
+        # because it requires you to know the expected output ("str", "dict", ...)
+        # and pass that in as a param, where that could really be abstracted away from
+        # the user. For that reason, it is currently set to False -- this may change
+        # in the future if desired.
+        if isinstance(self.data, urllib3.HTTPResponse):
+            return json.loads(self.data.data)
+
         # the response data comes back as a string formatted as a python
         # dictionary might be, where the inner quotes are single quotes
         # (') instead of the double quotes (") expected by JSON standard.
